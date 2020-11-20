2079b397fbc1201c731967991ea560b3816051bd
Susanna Landström <susannalandstrom@SusannaLandstromMBP2019.local>
2020-11-20 13:19:15 +0200

    add FormControl

---
+++ src/components/FormControl.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import {FormControl as MuiFormControl, FormControlProps as MuiFormControlProps, createStyles, makeStyles, Theme} from '@material-ui/core';
+
+const useStyles = makeStyles<Theme>(theme => createStyles({
+}));
+
+export interface FormControlProps extends MuiFormControlProps {}
+
+const FormControl: FunctionComponent<FormControlProps> = ({ ...props }) => {
+    const classes = useStyles();
+
+    return (
+        <MuiFormControl {...props} classes={{ root: classes.root }} />
+    )
+};
+
+export default FormControl;
