efba02b04325a4f709930145d441b81cf3baa897
Susanna Landström <susannalandstrom@SusannaLandstromMBP2019.local>
2020-11-27 16:40:28 +0200

    add InputLabel

---
+++ src/components/InputLabel.tsx
@@ -0,0 +1,21 @@
+import React, { FunctionComponent } from 'react';
+import {InputLabel as MuiInputLabel, InputLabelProps as MuiInputLabelProps, createStyles, makeStyles, Theme} from '@material-ui/core';
+
+const useStyles = makeStyles<Theme>(theme => createStyles({
+    root: {
+        color: theme.palette.grey[400],
+        letterSpacing: '0.15px'
+    },
+}));
+
+export interface InputLabelProps extends MuiInputLabelProps {}
+
+const InputLabel: FunctionComponent<InputLabelProps> = ({ ...props }) => {
+    const classes = useStyles();
+
+    return (
+        <MuiInputLabel {...props} classes={{ root: classes.root }} />
+    )
+};
+
+export default InputLabel;
