0e09f379cb7d1a15d61e3614b7ccd5a7142425da
Susanna Landström <susannalandstrom@SusannaLandstromMBP2019.local>
2020-11-27 16:57:18 +0200

    add TextField

---
+++ src/components/TextField.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from 'react';
+import {TextField as MuiTextField, OutlinedTextFieldProps, createStyles, makeStyles, Theme} from '@material-ui/core';
+
+const useStyles = makeStyles<Theme>(theme => createStyles({
+    root: {
+        minWidth: '220px',
+        '& label': {
+            color: theme.palette.grey[400],
+            letterSpacing: '0.15px',
+        },
+        '& .MuiFormLabel-root.Mui-disabled': {
+            color: theme.palette.grey[400],
+        },
+        '& .MuiInputLabel-shrink': {
+            transform: 'translate(0, -20px) scale(1)'
+        },
+        '& .MuiInputBase-root.Mui-disabled': {
+            backgroundColor: 'rgba(122, 131, 137, 0.08)'
+        },
+        '& .MuiFormHelperText-root': {
+            letterSpacing: '0.15px'
+        },
+        '& .MuiFormHelperText-contained': {
+            marginLeft: 0,
+        },
+    }
+}));
+
+export interface TextFieldProps extends OutlinedTextFieldProps {}
+
+const TextField: FunctionComponent<TextFieldProps> = props => {
+    const classes = useStyles();
+
+    return (
+        <MuiTextField {...props} classes={{ root: classes.root }} />
+    )
+};
+
+export default TextField;
---
+++ src/stories/TextField.stories.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import TextField, { TextFieldProps } from '../components/TextField';
+import ThemeSelector from "../themes/ThemeSelector";
+import InputAdornment from '../components/InputAdornment';
+
+export default {
+  title: 'Components/TextField',
+  component: TextField,
+} as Meta;
+
+const Template: Story<TextFieldProps> = (args) => <ThemeSelector><TextField {...args} /></ThemeSelector>;
+
+export const Default = Template.bind({});
+Default.args = {
+  margin: 'dense',
+  variant: 'outlined',
+  label: "Label",
+  placeholder: "Placeholder",
+  disabled: false,
+  error: false,
+  InputLabelProps: { shrink: true },
+  InputProps: { notched: false },
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  margin: 'normal',
+  variant: 'outlined',
+  label: "Label",
+  placeholder: "Placeholder",
+  disabled: false,
+  error: false,
+  InputLabelProps: { shrink: true },
+  InputProps: { notched: false },
+};
+
+export const HelperText = Template.bind({});
+HelperText.args = {
+  margin: 'dense',
+  variant: 'outlined',
+  label: "Label",
+  placeholder: "Placeholder",
+  helperText: "Helper text",
+  disabled: false,
+  error: false,
+  InputLabelProps: { shrink: true },
+  InputProps: { notched: false },
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  margin: 'dense',
+  variant: 'outlined',
+  label: "Label",
+  placeholder: "Placeholder",
+  helperText: "Helper text",
+  disabled: false,
+  error: true,
+  InputLabelProps: { shrink: true },
+  InputProps: { notched: false },
+};
+
+export const StartAdornment = Template.bind({});
+StartAdornment.args = {
+  margin: 'dense',
+  variant: 'outlined',
+  label: "Label",
+  placeholder: "Placeholder",
+  helperText: "Helper text",
+  disabled: false,
+  error: false,
+  InputLabelProps: { shrink: true },
+  InputProps: { notched: false, startAdornment: <InputAdornment position="start">Kg</InputAdornment> },
+};
+
+export const EndAdornment = Template.bind({});
+EndAdornment.args = {
+  margin: 'dense',
+  variant: 'outlined',
+  label: "Label",
+  placeholder: "Placeholder",
+  helperText: "Helper text",
+  disabled: false,
+  error: false,
+  InputLabelProps: { shrink: true },
+  InputProps: { notched: false, endAdornment: <InputAdornment position="end">Kg</InputAdornment> },
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  margin: 'dense',
+  variant: 'outlined',
+  label: "Label",
+  placeholder: "Placeholder",
+  disabled: true,
+  InputLabelProps: { shrink: true },
+  InputProps: { notched: false, endAdornment: <InputAdornment position="end">Kg</InputAdornment> },
+};
