2bfc7d03e5040fbae6d654661257bad5eae7743d
Susanna Landström <susannalandstrom@SusannaLandstromMBP2019.local>
2020-11-27 16:40:55 +0200

    add InputAdornment

---
+++ src/components/InputAdornment.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react';
+import {InputAdornment as MuiInputAdornment, InputAdornmentProps as MuiInputAdornmentProps, createStyles, makeStyles, Theme} from '@material-ui/core';
+
+const useStyles = makeStyles<Theme>(theme => createStyles({
+    root: {
+        color: theme.palette.text.primary,
+    }
+}));
+
+export interface InputAdornmentProps extends MuiInputAdornmentProps {}
+
+const InputAdornment: FunctionComponent<InputAdornmentProps> = ({ ...props }) => {
+    const classes = useStyles();
+
+    return (
+        <MuiInputAdornment {...props} classes={{ root: classes.root }} />
+    )
+};
+
+export default InputAdornment;
