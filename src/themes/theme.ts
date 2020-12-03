f0d4a01d7efef841cfabf4651794fc65315b3903
Wilhelm Törnroos <wilhelm@lyyti.fi>
2020-12-03 11:21:37 +0200

    Add some WIP font stuff

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,4 +1,40 @@
 import { createMuiTheme } from "@material-ui/core/styles";
+import ObjektivMk1RgTff from "../fonts/ObjektivMk1_Rg.ttf";
+import ObjektivMk1MdTff from "../fonts/ObjektivMk1_Md.ttf";
+import ObjektivMk3RgTff from "../fonts/ObjektivMk3_Rg.ttf";
+
+const objektivMk1Rg = {
+    fontFamily: 'Objektiv MK1 Regular',
+    fontStyle: 'normal',
+    fontDisplay: 'swap' as const,
+    fontWeight: 400,
+    src: `
+        local('Objektiv MK1 Regular'),
+        url(/${ObjektivMk1RgTff}) format('truetype'),
+    `,
+};
+
+const objektivMk1Md = {
+    fontFamily: 'Objektiv MK1 Medium',
+    fontStyle: 'normal',
+    fontDisplay: 'swap' as const,
+    fontWeight: 500,
+    src: `
+        local('Objektiv MK1 Medium'),
+        url(/${ObjektivMk1MdTff}) format('truetype'),
+    `,
+};
+
+const objektivMk3Rg = {
+    fontFamily: 'Objektiv MK3 Regular',
+    fontStyle: 'normal',
+    fontDisplay: 'swap' as const,
+    fontWeight: 400,
+    src: `
+        local('Objektiv MK3 Regular'),
+        url(/${ObjektivMk3RgTff}) format('truetype'),
+    `,
+};
 
 export default createMuiTheme({
     palette: {
@@ -59,7 +95,7 @@ export default createMuiTheme({
         }
     },
     typography: {
-        fontFamily: 'Nunito Sans, sans-serif',
+        fontFamily: 'Objektiv MK1 Regular, sans-serif',
         button: {
           textTransform: 'none',
           fontWeight: 500,
@@ -75,5 +111,12 @@ export default createMuiTheme({
         MuiSwitch: {
             color: 'primary',
         }
-    }
+    },
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                '@font-face': [objektivMk1Rg, objektivMk1Md, objektivMk3Rg],
+            },
+        },
+    },
 });
