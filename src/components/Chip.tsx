e08f45d06816e60db6cf93208f861c59ca1c0284
Susanna Landström <susanna.landstrom@lyyti.com>
2021-01-13 11:25:12 +0200

    update chip to use only primary and secondary colors

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,7 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import {Chip as MuiChip, ChipProps as MuiChipProps} from '@material-ui/core';
 
-export interface ChipProps extends MuiChipProps {}
+export type ColorTypes =
+  | "primary"
+  | "secondary";
+
+export type ChipProps = MuiChipProps & { color: ColorTypes };
 
 const Chip: FunctionComponent<ChipProps> = props => {
 
