82314ccfb50fb95fff8b58be816b8351a12ed51b
Susanna Landström <susanna.landstrom@lyyti.com>
2021-01-14 14:30:09 +0200

    move styles to stepper

--- src/components/StepLabel.tsx
+++ src/components/StepLabel.tsx
@@ -1,31 +1,12 @@
 import React, { FunctionComponent } from 'react';
-import {StepLabel as MuiStepLabel, StepLabelProps as MuiStepLabelProps, createStyles, makeStyles, Theme} from '@material-ui/core';
-
-const useStyles = makeStyles<Theme>(theme => createStyles({
-    root: {
-        '& .MuiStepLabel-label.MuiStepLabel-completed': {
-            color: theme.palette.secondary.main
-        },
-        '& .MuiStepLabel-label.MuiStepLabel-active': {
-            color: theme.palette.secondary.main
-        },
-        '& .MuiStepLabel-label.MuiStepLabel-alternativeLabel': {
-            marginTop: '8px'
-        }
-    },
-    label: {
-        fontSize: '15px',
-        color: theme.palette.action.disabledBackground
-    },
-}));
+import {StepLabel as MuiStepLabel, StepLabelProps as MuiStepLabelProps} from '@material-ui/core';
 
 export interface StepLabelProps extends MuiStepLabelProps {}
 
 const StepLabel: FunctionComponent<StepLabelProps> = props => {
-    const classes = useStyles();
 
     return (
-        <MuiStepLabel {...props} classes={{ root: classes.root, label: classes.label }} />
+        <MuiStepLabel {...props} />
     )
 };
 
