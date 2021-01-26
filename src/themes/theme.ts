332d03333f95175b0b22a1ef55fb077310f82a38
Petro Silenius <petro.silenius@lyyti.com>
2021-01-26 15:47:57 +0200

    Remove extra bracket in theme.ts, include all typings in tsconfig

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -46,106 +46,106 @@ declare module '@material-ui/core/styles/createPalette' {
 }
 
 export default createMuiTheme({
-    palette: {
-        primary: {
-            main: '#045B56',
-            dark: '#034E49',
-            light: '#05756F',
-            contrastText: '#FFFFFF',
-        },
-        secondary: {
-            main: '#EE8B3A',
-            dark: '#DD6E13',
-            light: '#F2A96D',
-            contrastText: '#FFFFFF'
-        },
-        info: {
-            main: '#1F98CD',
-            dark: '#18769F',
-            light: '#7CCEE9',
-            contrastText: '#1F98CD'
-        },
-        error: {
-            main: '#D1492E',
-            dark: '#A33924',
-            light: '#F4A094',
-            contrastText: '#D1492E'
-        },
-        warning: {
-            main: '#F09000',
-            dark: '#B86D00',
-            light: '#FFC573',
-            contrastText: '#F09000'
-        },
-        success: {
-            main: '#50B54E',
-            dark: '#3E903C',
-            light: '#99DCA1',
-            contrastText: '#50B54E'
-        },
-        text: {
-            primary: "#192832",
-            secondary: '#7A8389',
-            disabled: 'rgba(122, 131, 137, 0.5)',
-        },
-        action: {
-            hover: 'rgba(122, 131, 137, 0.1)',
-            selected: 'rgba(122, 131, 137, 0.08)',
-            disabled: '#B8BCBF',
-            disabledBackground: 'rgba(122, 131, 137, 0.5)'
-        },
-        grey: {
-             "50": '#F9FAFB',
-             "100": '#E3E6E9',
-             "200": '#B1B9BE',
-             "300": '#7A8389',
-             "400": '#46545B',
-             "500": '#192832'
-        },
-        primaryStates: {
-            activeContained: 'rgba(255, 255, 255, 0.3)',
-            hover: 'rgba(4, 91, 86, 0.1)',
-            selected: 'rgba(4, 91, 86, 0.08)',
-            activeOutlined: 'rgba(4, 91, 86, 0.24)',
-            outlinedStroke: '#045B56',
-            disabledBg: '#739C9A'
-        },
+  palette: {
+    primary: {
+      main: '#045B56',
+      dark: '#034E49',
+      light: '#05756F',
+      contrastText: '#FFFFFF',
     },
-    typography: {
-        fontFamily: 'Objektiv MK1 Regular, sans-serif',
-        button: {
-          textTransform: 'none',
-          fontWeight: 500,
-        }
+    secondary: {
+      main: '#EE8B3A',
+      dark: '#DD6E13',
+      light: '#F2A96D',
+      contrastText: '#FFFFFF',
     },
-    props: {
-        MuiCheckbox: {
-            color: 'primary'
-        },
-        MuiRadio: {
-            color: 'primary'
-        },
-        MuiSwitch: {
-            color: 'primary'
-        },
-        MuiChip: {
-            color: 'primary'
-        },
-        MuiTextField: {
-            variant: 'outlined'
-        }
+    info: {
+      main: '#1F98CD',
+      dark: '#18769F',
+      light: '#7CCEE9',
+      contrastText: '#1F98CD',
     },
-    overrides: {
-        MuiCssBaseline: {
-            '@global': {
-                '@font-face': [objektivMk1Rg, objektivMk1Md, objektivMk3Rg],
-            },
-        },
-        MuiPaper: {
-            elevation1: {
-                boxShadow: '0.79px 6.95px 11px rgba(0, 0, 0, 0.0096), 0.52px 4.53px 6.44px rgba(0, 0, 0, 0.0157), 0.31px 2.76px 3.5px rgba(0, 0, 0, 0.02), 0.17px 1.52px 1.79px rgba(0, 0, 0, 0.0243), 0.08px 0.72px 0.9px rgba(0, 0, 0, 0.0304), 0.03px 0.25px 0.43px rgba(0, 0, 0, 0.04)'
-            }
-        }
+    error: {
+      main: '#D1492E',
+      dark: '#A33924',
+      light: '#F4A094',
+      contrastText: '#D1492E',
+    },
+    warning: {
+      main: '#F09000',
+      dark: '#B86D00',
+      light: '#FFC573',
+      contrastText: '#F09000',
+    },
+    success: {
+      main: '#50B54E',
+      dark: '#3E903C',
+      light: '#99DCA1',
+      contrastText: '#50B54E',
+    },
+    text: {
+      primary: '#192832',
+      secondary: '#7A8389',
+      disabled: 'rgba(122, 131, 137, 0.5)',
+    },
+    action: {
+      hover: 'rgba(122, 131, 137, 0.1)',
+      selected: 'rgba(122, 131, 137, 0.08)',
+      disabled: '#B8BCBF',
+      disabledBackground: 'rgba(122, 131, 137, 0.5)',
+    },
+    grey: {
+      '50': '#F9FAFB',
+      '100': '#E3E6E9',
+      '200': '#B1B9BE',
+      '300': '#7A8389',
+      '400': '#46545B',
+      '500': '#192832',
+    },
+    primaryStates: {
+      activeContained: 'rgba(255, 255, 255, 0.3)',
+      hover: 'rgba(4, 91, 86, 0.1)',
+      selected: 'rgba(4, 91, 86, 0.08)',
+      activeOutlined: 'rgba(4, 91, 86, 0.24)',
+      outlinedStroke: '#045B56',
+      disabledBg: '#739C9A',
+    },
+  },
+  typography: {
+    fontFamily: 'Objektiv MK1 Regular, sans-serif',
+    button: {
+      textTransform: 'none',
+      fontWeight: 500,
+    },
+  },
+  props: {
+    MuiCheckbox: {
+      color: 'primary',
+    },
+    MuiRadio: {
+      color: 'primary',
+    },
+    MuiSwitch: {
+      color: 'primary',
+    },
+    MuiChip: {
+      color: 'primary',
+    },
+    MuiTextField: {
+      variant: 'outlined',
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [objektivMk1Rg, objektivMk1Md, objektivMk3Rg],
+      },
+    },
+    MuiPaper: {
+      elevation1: {
+        boxShadow:
+          '0.79px 6.95px 11px rgba(0, 0, 0, 0.0096), 0.52px 4.53px 6.44px rgba(0, 0, 0, 0.0157), 0.31px 2.76px 3.5px rgba(0, 0, 0, 0.02), 0.17px 1.52px 1.79px rgba(0, 0, 0, 0.0243), 0.08px 0.72px 0.9px rgba(0, 0, 0, 0.0304), 0.03px 0.25px 0.43px rgba(0, 0, 0, 0.04)',
+      },
     },
   },
 });
