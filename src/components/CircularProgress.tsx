099ef01a26b52fdc7c88b1b341695f9d4d673a7b
Susanna Landström <susanna.landstrom@lyyti.com>
2021-02-23 08:32:08 +0200

    fix issues mentioned in PR

--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -2,61 +2,38 @@ import React, { FunctionComponent } from 'react';
 import {
   CircularProgress as MuiCircularProgress,
   CircularProgressProps as MuiCircularProgressProps,
-  createStyles,
   makeStyles,
-  Theme,
 } from '@material-ui/core';
 
-const useStyles = makeStyles<Theme>(() =>
-  createStyles({
-    root: {},
-    svg: {
-      transform: 'rotate(90deg)',
+const useStyles = makeStyles({
+  svg: {
+    transform: 'rotate(90deg)',
+  },
+  indeterminate: {
+    animation: 'circular-rotate 2s ease infinite',
+  },
+  '@keyframes circular-rotate': {
+    '0%': {
+      transform: 'rotate(0deg)',
     },
-    indeterminate: {
-      animation: 'circular-rotate 2s ease infinite',
+    '100%': {
+      transform: 'rotate(360deg)',
     },
-    '@-webkit-keyframes circular-rotate': {
-      '0%': {
-        transform: 'rotate(0deg)',
-      },
-      '100%': {
-        transform: 'rotate(360deg)',
-      },
+  },
+  circleIndeterminate: {
+    animation: `$circular-dash 2s ease-in-out infinite`,
+  },
+  '@keyframes circular-dash': {
+    '0%': {
+      strokeDasharray: '1px, 200px',
+      strokeDashoffset: '0px',
     },
-    '@keyframes circular-rotate': {
-      '0%': {
-        transform: 'rotate(0deg)',
-      },
-      '100%': {
-        transform: 'rotate(360deg)',
-      },
+    '100%': {
+      strokeDasharray: '100px, 200px',
+      strokeDashoffset: '-125px',
     },
-    circleIndeterminate: {
-      animation: `$circular-dash 2s ease-in-out infinite`,
-    },
-    '@-webkit-keyframes circular-dash': {
-      '0%': {
-        strokeDasharray: '1px, 200px',
-        strokeDashoffset: '0px',
-      },
-      '100%': {
-        strokeDasharray: '100px, 200px',
-        strokeDashoffset: '-125px',
-      },
-    },
-    '@keyframes circular-dash': {
-      '0%': {
-        strokeDasharray: '1px, 200px',
-        strokeDashoffset: '0px',
-      },
-      '100%': {
-        strokeDasharray: '100px, 200px',
-        strokeDashoffset: '-125px',
-      },
-    },
-  })
-);
+  },
+});
 
 export type CircularProgressProps = MuiCircularProgressProps;
 
@@ -69,7 +46,6 @@ const CircularProgress: FunctionComponent<CircularProgressProps> = (props) => {
       classes={{
         circleIndeterminate: classes.circleIndeterminate,
         indeterminate: classes.indeterminate,
-        root: classes.root,
         svg: classes.svg,
       }}
     />
