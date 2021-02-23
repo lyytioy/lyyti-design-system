7859d6a376ec9a1fa96b2c49bd00533c56ee9dfd
Susanna Landström <susanna.landstrom@lyyti.com>
2021-02-23 14:00:12 +0200

    remove button style css file

--- src/themes/ThemeSelector.tsx
+++ src/themes/ThemeSelector.tsx
@@ -2,7 +2,6 @@ import React, { FunctionComponent, ReactNode } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Theme from './theme';
 import { CssBaseline } from '@material-ui/core';
-import './buttonStyles.css';
 
 interface ThemeSelectorProps {
   children: ReactNode;
