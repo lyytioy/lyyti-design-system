6995cbfabf915364ccfb3d91be2b17a4950afc40
Petro Silenius <petro.silenius@lyyti.com>
2021-03-02 13:32:52 +0200

    Add initial TimePicker, useAutocomplete is causing errors still

---
+++ src/stories/Inputs/TimePicker.stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import ThemeSelector from '../../themes/ThemeSelector';
+import TimePicker, { TimePickerProps } from '../../components/TimePicker';
+
+export default {
+  title: 'Components/Inputs/TimePicker',
+  component: TimePicker,
+} as Meta;
+
+const Template: Story<TimePickerProps> = (args) => (
+  <ThemeSelector>
+    <TimePicker {...args} />
+  </ThemeSelector>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
