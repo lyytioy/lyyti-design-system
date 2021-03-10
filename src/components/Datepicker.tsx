4cc9e945181c52bc38e25065f6b0ac3206720e97
Wilhelm Törnroos <wilhelm@lyyti.fi>
2021-03-10 17:17:54 +0200

    Update date picker

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -1,29 +1,141 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'react-dates/initialize';
 import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 import moment from 'moment';
+import InputLabel from './InputLabel';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import Calendar from './icons/Calendar';
+import ChevronLeft from './icons/ChevronLeft';
+import ChevronRight from './icons/ChevronRight';
 
-const Datepicker = (): JSX.Element => {
-  const [date, setDate] = useState<moment.Moment | null>(null);
+const useStyles = makeStyles<Theme>((theme) => {
+  const borderColor =
+    theme.palette.type === 'light' ? 'rgba(0, 0, 0, 0.23)' : 'rgba(255, 255, 255, 0.23)';
+
+  return createStyles({
+    root: {
+      ...theme.typography.body1,
+      color: theme.palette.text.primary,
+      display: 'inline-flex',
+      flexDirection: 'column',
+      minWidth: 220,
+      verticalAlign: 'top',
+      '& label': {
+        color: theme.palette.grey[400],
+        letterSpacing: '0.15px',
+        lineHeight: '26px',
+      },
+      '& .DateInput': {
+        background: 'none',
+        width: 'initial',
+      },
+      '& .DateInput_input': {
+        background: 'none',
+        border: 0,
+        boxSizing: 'content-box',
+        color: 'currentColor',
+        font: 'inherit',
+        height: '1.1876em',
+        lineHeight: 'inherit',
+        padding: '10.5px 14px',
+        width: 'initial',
+      },
+      '& .SingleDatePickerInput': {
+        background: 'none',
+      },
+      '& .SingleDatePickerInput__withBorder': {
+        borderColor: borderColor,
+        borderRadius: theme.shape.borderRadius,
+        boxSizing: 'border-box',
+      },
+      '& .SingleDatePickerInput__withBorder:hover': {
+        borderColor: theme.palette.primary.main,
+      },
+      '& .SingleDatePickerInput_calendarIcon': {
+        color: theme.palette.grey[400],
+        lineHeight: 1,
+        marginRight: 8,
+        padding: 0,
+      },
+    },
+    focused: {
+      '& label': {
+        color: theme.palette.text.primary,
+      },
+      '& .SingleDatePickerInput__withBorder': {
+        borderColor: theme.palette.primary.main,
+      },
+      '& .SingleDatePickerInput_calendarIcon': {
+        color: theme.palette.primary.main,
+      },
+    },
+    navButton: {
+      lineHeight: 1,
+      padding: '8px',
+      position: 'absolute',
+      top: 16,
+    },
+  });
+});
+
+export interface DatepickerProps {
+  date: moment.Moment | null;
+  id: string;
+  label?: string;
+  locale?: string;
+  margin?: 'dense' | 'normal';
+  numberOfMonths?: number;
+  onDateChange: (date: moment.Moment | null) => void;
+}
+
+const Datepicker = ({
+  date,
+  id,
+  label,
+  locale = 'en',
+  margin = 'dense',
+  numberOfMonths = 1,
+  onDateChange,
+}: DatepickerProps): JSX.Element => {
+  const classes = useStyles();
+
+  useEffect(() => {
+    moment.locale(locale);
+  }, [locale]);
+
   const [focused, setFocused] = useState(false);
-
-  const handleDateChange = (obj: typeof date) => {
-    setDate(obj);
-  };
 
   const handleFocusChange = (arg: { focused: typeof focused }) => {
     setFocused(arg.focused);
   };
 
   return (
-    <SingleDatePicker
-      id="foo"
-      date={date}
-      focused={focused}
-      onDateChange={handleDateChange}
-      onFocusChange={handleFocusChange}
-    />
+    <div className={`${classes.root} ${focused ? classes.focused : ''}`}>
+      {label && <InputLabel htmlFor={id}>{label}</InputLabel>}
+      <SingleDatePicker
+        id={id}
+        date={date}
+        focused={focused}
+        onDateChange={onDateChange}
+        onFocusChange={handleFocusChange}
+        customInputIcon={<Calendar />}
+        inputIconPosition="after"
+        numberOfMonths={numberOfMonths}
+        navPrev={
+          <span className={classes.navButton} style={{ left: 22 }}>
+            <ChevronLeft />
+          </span>
+        }
+        navNext={
+          <span className={classes.navButton} style={{ right: 22 }}>
+            <ChevronRight />
+          </span>
+        }
+        hideKeyboardShortcutsPanel
+        firstDayOfWeek={1}
+      />
+    </div>
   );
 };
 
--- src/stories/Inputs/Datepicker.stories.tsx
+++ src/stories/Inputs/Datepicker.stories.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Datepicker from '../../components/Datepicker';
+import Datepicker, { DatepickerProps } from '../../components/Datepicker';
 import ThemeSelector from '../../themes/ThemeSelector';
+import moment from 'moment';
 
 export default {
   title: 'Components/Inputs/Date Picker',
   component: Datepicker,
+  parameters: {
+    layout: 'padded',
+  },
 } as Meta;
 
-const Template: Story = (args) => (
+const Template: Story<DatepickerProps> = (args) => (
   <ThemeSelector>
     <Datepicker {...args} />
   </ThemeSelector>
 );
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  date: moment(),
+  id: 'datepicker',
+  locale: 'en-US',
+  label: 'Label',
+  onDateChange: (date) => console.log,
+};
