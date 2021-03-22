d75a41eee763d39577b996b727d393ae5d692f7a
Petro Silenius <petro.silenius@lyyti.com>
2021-03-22 16:39:31 +0200

    Add initial icon list

--- src/stories/DataDisplay/Icons.stories.tsx
+++ src/stories/DataDisplay/Icons.stories.tsx
@@ -1,18 +1,76 @@
+import { ChangeEvent, useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import ThemeSelector from 'themes/ThemeSelector';
-import CogIcon from 'components/icons/Cog';
-import { SvgIconProps } from '@material-ui/core';
+import { SvgIconProps, makeStyles, Theme, createStyles } from '@material-ui/core';
+import TextField from 'components/TextField';
+import Grid from 'components/Grid';
+
+import Cog from 'components/icons/Cog';
+import Star from 'components/icons/Star';
 
 export default {
   title: 'Components/Data Display/Icons',
-  component: CogIcon,
+  component: Cog,
+  argTypes: {
+    htmlColor: {
+      control: 'color',
+    },
+  },
 } as Meta;
 
-const Template: Story<SvgIconProps> = (args) => (
-  <ThemeSelector>
-    <CogIcon {...args} />
-  </ThemeSelector>
+const iconsList = [
+  { name: 'Star', component: Star },
+  { name: 'Cog', component: Cog },
+];
+
+const useStyles = makeStyles<Theme>((theme) =>
+  createStyles({
+    iconDiv: {
+      backgroundColor: theme.palette.grey[200],
+      borderRadius: '10px',
+      padding: '10px',
+      display: 'grid',
+      placeItems: 'center',
+    },
+  })
 );
 
-export const Cog = Template.bind({});
-Cog.args = {};
+const Template: Story<SvgIconProps> = (args) => {
+  const classes = useStyles();
+  const [search, setSearch] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  console.log(Object.entries(iconsList));
+  return (
+    <ThemeSelector>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <TextField
+            onChange={handleChange}
+            value={search}
+            variant="outlined"
+            placeholder="Search"
+          />
+        </Grid>
+        {iconsList
+          .filter(({ name }) => name.toLowerCase().includes(search.toLowerCase()))
+          .map((icon, index) => {
+            return (
+              <Grid item xs={2} key={index}>
+                <div className={classes.iconDiv}>
+                  <icon.component {...args} />
+                  <span>{icon.name}</span>
+                </div>
+              </Grid>
+            );
+          })}
+      </Grid>
+    </ThemeSelector>
+  );
+};
+
+export const All = Template.bind({});
+All.args = { color: undefined, htmlColor: undefined, fontSize: undefined };
