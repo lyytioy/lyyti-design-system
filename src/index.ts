5b24c91528b4a35c9a1a1ddf6dc9a71c96f16237
Wilhelm Törnroos <wilhelm@lyyti.fi>
2021-03-23 16:30:03 +0200

    Make changes to enable building and using as a dependency

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
 // https://www.learnstorybook.com/design-systems-for-developers/react/en/distribute/
 
-export * from './components/Button';
+import * as Cog from './components/icons/Cog';
+import * as Star from './components/icons/Star';
+
+export { Cog, Star };
+
+export { default as ThemeSelector } from './themes/ThemeSelector';
+
+export { default as Alert } from './components/Alert';
+export { default as AlertTitle } from './components/AlertTitle';
+export { default as Avatar } from './components/Avatar';
+export { default as Button } from './components/Button';
+export { default as Card } from './components/Card';
+export { default as CardActions } from './components/CardActions';
+export { default as CardContent } from './components/CardContent';
+export { default as CardHeader } from './components/CardHeader';
+export { default as Checkbox } from './components/Checkbox';
+export { default as Chip } from './components/Chip';
+export { default as CircularProgress } from './components/CircularProgress';
+export { default as ClickAwayListener } from './components/ClickAwayListener';
+export { default as DataGrid } from './components/DataGrid';
+export { default as Dialog } from './components/Dialog';
+export { default as DropdownMenu } from './components/DropdownMenu';
+export { default as FormControl } from './components/FormControl';
+export { default as FormControlLabel } from './components/FormControlLabel';
+export { default as FormGroup } from './components/FormGroup';
+export { default as FormLabel } from './components/FormLabel';
+export { default as Grid } from './components/Grid';
+export { default as Grow } from './components/Grow';
+export { default as InputAdornment } from './components/InputAdornment';
+export { default as InputLabel } from './components/InputLabel';
+export { default as Link } from './components/Link';
+export { default as MenuItem } from './components/MenuItem';
+export { default as MenuList } from './components/MenuList';
+export { default as Pagination } from './components/Pagination';
+export { default as Paper } from './components/Paper';
+export { default as Popper } from './components/Popper';
+export { default as Radio } from './components/Radio';
+export { default as Select } from './components/Select';
+export { default as Skeleton } from './components/Skeleton';
+export { default as StepButton } from './components/StepButton';
+export { default as StepConnector } from './components/StepConnector';
+export { default as StepLabel } from './components/StepLabel';
+export { default as Stepper } from './components/Stepper';
+export { default as Switch } from './components/Switch';
+export { default as Tab } from './components/Tab';
+export { default as Tabs } from './components/Tabs';
+export { default as TextField } from './components/TextField';
+export { default as TimePicker } from './components/TimePicker';
+export { default as Tooltip } from './components/Tooltip';
