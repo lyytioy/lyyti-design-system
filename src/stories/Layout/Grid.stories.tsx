f994dfcb981a0ab08b3ddd4c310421620745bde8
Petro Silenius <petro.silenius@lyyti.com>
2021-03-31 15:15:44 +0300

    Add initial documentation to Grid

--- src/stories/Layout/Grid.stories.tsx
+++ src/stories/Layout/Grid.stories.tsx
@@ -6,10 +6,69 @@ import { Rect } from '../DataDisplay/Skeleton.stories';
 export default {
   title: 'Components/Layout/Grid',
   component: Grid,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Grids are a great framework to help us put together a clean, well-aligned interface; one that helps our users interact with Lyyti with ease.',
+      },
+    },
+  },
   argTypes: {
     ref: { table: { disable: true } },
     spacing: {
-      control: { type: 'select', options: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, undefined] },
+      control: { type: 'number', options: { min: 0, max: 10 } },
+      table: {
+        defaultValue: { summary: 0 },
+      },
+      description:
+        'Defines the space between the type item component. It can only be used on a type container component.',
+    },
+    xs: {
+      control: {
+        type: 'number',
+        min: 1,
+        max: 12,
+      },
+      table: { defaultValue: { summary: false } },
+      description:
+        "Defines the number of grids the component is going to use. It's applied for the xs breakpoint and wider screens if not overridden.",
+    },
+    sm: {
+      control: {
+        type: 'number',
+        min: 1,
+        max: 12,
+      },
+      table: { defaultValue: { summary: false } },
+    },
+    md: {
+      control: {
+        type: 'number',
+        min: 1,
+        max: 12,
+      },
+      table: { defaultValue: { summary: false } },
+    },
+    lg: {
+      control: {
+        type: 'number',
+        min: 1,
+        max: 12,
+      },
+      table: { defaultValue: { summary: false } },
+    },
+    xl: {
+      control: {
+        type: 'number',
+        min: 1,
+        max: 12,
+      },
+      table: { defaultValue: { summary: false } },
+    },
+    justify: {
+      description:
+        'Defines the justify-content style property. It is applied for all screen sizes.',
     },
   },
 } as Meta;
