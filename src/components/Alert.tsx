29cf4ac2c97eeb46a067dc53367efad641f0d192
Susanna Landström <susanna.landstrom@lyyti.com>
2021-04-14 12:38:14 +0300

    convert default props to default parameters

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,24 +1,17 @@
 import { Alert as MuiAlert, AlertProps as MuiAlertProps } from '@material-ui/lab';
 
 export interface AlertProps extends MuiAlertProps {
-  variant: 'standard' | 'filled' | 'outlined';
+  color?: 'success' | 'info' | 'warning' | 'error';
   severity: 'success' | 'info' | 'warning' | 'error';
-  color: 'success' | 'info' | 'warning' | 'error';
+  variant: 'standard' | 'filled' | 'outlined';
 }
 
-const Alert = (props: AlertProps): JSX.Element => {
-  return <MuiAlert {...props} />;
-};
-
-Alert.defaultProps = {
-  children: 'Alert content in here',
-  closeText: 'Close',
-  variant: 'standard',
-  severity: 'success',
-  onClose: undefined,
-  action: undefined,
-  color: undefined,
-  icon: undefined,
+const Alert = ({
+  severity = 'success',
+  variant = 'standard',
+  ...props
+}: AlertProps): JSX.Element => {
+  return <MuiAlert severity={severity} variant={variant} {...props} />;
 };
 
 export default Alert;
--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -4,37 +4,44 @@ import Slide from '@material-ui/core/Slide';
 import { TransitionProps } from '@material-ui/core/transitions';
 
 export interface SnackbarProps extends MuiSnackbarProps {
-  severity?: 'success' | 'info' | 'warning' | 'error' | undefined;
-  variant?: 'standard' | 'filled' | 'outlined' | undefined;
-  direction?: 'right' | 'left' | 'up' | 'down' | undefined;
+  anchorOrigin: {
+    horizontal: 'center' | 'left' | 'right';
+    vertical: 'bottom' | 'top';
+  };
+  autoHideDuration: number;
+  color?: 'success' | 'info' | 'warning' | 'error';
+  direction: 'right' | 'left' | 'up' | 'down';
+  severity: 'success' | 'info' | 'warning' | 'error';
+  variant: 'standard' | 'filled' | 'outlined';
 }
 
-const Snackbar = (props: SnackbarProps): JSX.Element => {
+const Snackbar = ({
+  anchorOrigin = {
+    vertical: 'top',
+    horizontal: 'right',
+  },
+  autoHideDuration = 5000,
+  color,
+  direction = 'left',
+  severity = 'success',
+  variant = 'standard',
+  ...props
+}: SnackbarProps): JSX.Element => {
   return (
     <MuiSnackbar
       {...props}
+      anchorOrigin={anchorOrigin}
+      autoHideDuration={autoHideDuration}
       TransitionComponent={(params: TransitionProps) => (
-        <Slide {...params} direction={props.direction} />
+        <Slide {...params} direction={direction} />
       )}>
       <div>
-        <Alert severity={props.severity} variant={props.variant}>
+        <Alert color={color} severity={severity} variant={variant}>
           {props.message}
         </Alert>
       </div>
     </MuiSnackbar>
   );
-};
-
-Snackbar.defaultProps = {
-  anchorOrigin: {
-    vertical: 'top',
-    horizontal: 'right',
-  },
-  autoHideDuration: 6000,
-  message: 'Success message',
-  severity: 'success',
-  variant: 'standard',
-  direction: 'left',
 };
 
 export default Snackbar;
