903aa7394cdea38de4f8efe54606995794a30a40
Susanna Landström <susanna.landstrom@lyyti.com>
2021-07-08 15:45:43 +0300

    update stories

--- src/stories/DataDisplay/Typography.stories.tsx
+++ src/stories/DataDisplay/Typography.stories.tsx
@@ -13,7 +13,7 @@ export default {
     },
   },
   args: {
-    children: 'Lorem ipsum',
+    children: 'Some text',
   },
 } as Meta;
 
@@ -24,7 +24,10 @@ const Template: Story<TypographyProps> = (args) => (
 );
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  children:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean felis turpis, bibendum et purus a, lacinia ornare nunc. In posuere sapien eu ultrices tristique. Etiam libero velit, egestas gravida diam at, maximus pulvinar felis. Praesent ut vestibulum velit. Duis sapien sapien, consequat ut luctus vitae, tincidunt quis purus. Pellentesque iaculis faucibus sapien vitae tincidunt. Praesent imperdiet libero et nisi ullamcorper convallis. Duis porta massa molestie tellus aliquet efficitur. In eu tristique turpis, eu convallis velit. Sed id lacus massa. Nullam sed mauris mattis, consequat odio eu, porttitor arcu. Praesent laoreet congue nisl, a hendrerit eros congue vel. Cras quis placerat lorem, ut iaculis quam. Phasellus sit amet ligula egestas, malesuada ante eu, bibendum mi. Phasellus porttitor lectus eget cursus rhoncus. Vivamus vitae odio a risus euismod rutrum sed id tortor.',
+};
 
 export const H1 = Template.bind({});
 H1.args = {
