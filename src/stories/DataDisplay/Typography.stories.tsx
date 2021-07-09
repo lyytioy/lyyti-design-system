38e7e0f1873af6a396324523dc0b03f3cac5e0ea
Susanna Landström <susanna.landstrom@lyyti.com>
2021-07-09 08:27:32 +0300

    update stories

--- src/stories/DataDisplay/Typography.stories.tsx
+++ src/stories/DataDisplay/Typography.stories.tsx
@@ -66,6 +66,9 @@ export default {
         },
       },
     },
+    variant: {
+      description: 'Applies the theme typography styles.',
+    },
   },
   args: {
     children: 'Some text',
@@ -87,8 +90,9 @@ Default.args = {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean felis turpis, bibendum et purus a, lacinia ornare nunc. In posuere sapien eu ultrices tristique. Etiam libero velit, egestas gravida diam at, maximus pulvinar felis. Praesent ut vestibulum velit. Duis sapien sapien, consequat ut luctus vitae, tincidunt quis purus. Pellentesque iaculis faucibus sapien vitae tincidunt. Praesent imperdiet libero et nisi ullamcorper convallis. Duis porta massa molestie tellus aliquet efficitur. In eu tristique turpis, eu convallis velit. Sed id lacus massa. Nullam sed mauris mattis, consequat odio eu, porttitor arcu. Praesent laoreet congue nisl, a hendrerit eros congue vel. Cras quis placerat lorem, ut iaculis quam. Phasellus sit amet ligula egestas, malesuada ante eu, bibendum mi. Phasellus porttitor lectus eget cursus rhoncus. Vivamus vitae odio a risus euismod rutrum sed id tortor.',
 };
 
-export const H1 = Template.bind({});
-H1.args = {
+export const pStyledAsH1 = Template.bind({});
+pStyledAsH1.args = {
   variant: 'h1',
   component: 'p',
+  color: 'primary',
 };
