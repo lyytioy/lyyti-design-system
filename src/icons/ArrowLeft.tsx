8998b49c35cab5dfaa486d1059e23fb5fdccc33e
Petro Silenius <petro.silenius@lyyti.com>
2021-07-12 14:06:57 +0300

    Generate icons using the templateSvgIcon.js template

--- src/icons/ArrowLeft.tsx
+++ src/icons/ArrowLeft.tsx
@@ -1,27 +1,21 @@
 import * as React from 'react';
 import SvgIcon from '@material-ui/core/SvgIcon';
 
-const KeyboardArrowLeft = (props: any) =>
+const ArrowLeft = (props: any) =>
   React.createElement(
     SvgIcon,
     props,
     <defs>
       <style>
         {
-          `
-          .keyboard-arrow-left_svg__a {
-            fill: none;
-            stroke: currentColor;
-            stroke-linecap: round;
-            stroke-linejoin: round;
-            stroke-width: 1.5px
-          }
-          `
+          '.keyboard-arrow-left_svg__a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px}'
         }
       </style>
     </defs>,
-    <path className="keyboard-arrow-left_svg__a" d="M23.25 12.497L0.75 12.497" />,
-    <path className="keyboard-arrow-left_svg__a" d="M4.5 16.247L0.75 12.497 4.5 8.747" />,
+    <path
+      className="keyboard-arrow-left_svg__a"
+      d="M23.25 12.497H.75M4.5 16.247l-3.75-3.75 3.75-3.75"
+    />
   );
 
-export default KeyboardArrowLeft;
+export default ArrowLeft;
--- src/icons/ArrowRight.tsx
+++ src/icons/ArrowRight.tsx
@@ -1,27 +1,21 @@
 import * as React from 'react';
 import SvgIcon from '@material-ui/core/SvgIcon';
 
-const KeyboardArrowRight = (props: any) =>
+const ArrowRight = (props: any) =>
   React.createElement(
     SvgIcon,
     props,
     <defs>
       <style>
         {
-          `
-          .keyboard-arrow-right_svg__a {
-            fill: none;
-            stroke: currentColor;
-            stroke-linecap: round;
-            stroke-linejoin: round;
-            stroke-width: 1.5px
-          }
-          `
+          '.keyboard-arrow-right_svg__a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px}'
         }
       </style>
     </defs>,
-    <path className="keyboard-arrow-right_svg__a" d="M23.25 12.497L0.75 12.497" />,
-    <path className="keyboard-arrow-right_svg__a" d="M19.5 16.247L23.25 12.497 19.5 8.747" />,
+    <path
+      className="keyboard-arrow-right_svg__a"
+      d="M23.25 12.497H.75M19.5 16.247l3.75-3.75-3.75-3.75"
+    />
   );
 
-export default KeyboardArrowRight;
+export default ArrowRight;
