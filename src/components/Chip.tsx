058ed0e6246d34feda9552296dd01351b2c32fb6
Susanna Landström <susanna.landstrom@lyyti.com>
2021-08-06 12:03:20 +0300

    update deleteIcon colors

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,4 +1,24 @@
-import { Chip as MuiChip, ChipProps as MuiChipProps } from '@material-ui/core';
+import {
+  Chip as MuiChip,
+  ChipProps as MuiChipProps,
+  createStyles,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+
+const useStyles = makeStyles<Theme>((theme) =>
+  createStyles({
+    root: {
+      '& .MuiChip-root': {
+        backgroundColor: theme.palette.primary.main,
+      },
+      '& .MuiChip-deleteIconColorPrimary': {
+        color: 'rgba(255, 255, 255, 0.7)',
+      },
+      '& .MuiChip-deleteIconColorSecondary': {},
+    },
+  })
+);
 
 export interface ChipProps extends MuiChipProps {
   color?: 'primary' | 'secondary';
@@ -14,14 +34,16 @@ const Chip = ({
   disabled = false,
   ...props
 }: ChipProps): JSX.Element => {
+  const classes = useStyles();
   return (
     <MuiChip
+      {...props}
+      classes={{ root: classes.root }}
       color={color}
       variant={variant}
       size={size}
       clickable={clickable}
       disabled={disabled}
-      {...props}
     />
   );
 };
