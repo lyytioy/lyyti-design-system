d43932c9bd8dcfd33d98185018e7d089b138b82c
Dusan Turajlic <dusan.turajlic@lyyti.com>
2022-01-12 17:42:53 +0200

    Updates README with some more detaild instructions

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import 'react-dates/initialize';
+import '../styles/datepicker.css';
 import { DateRangePicker, SingleDatePicker, FocusedInputShape } from 'react-dates';
 import moment from 'moment';
 import 'moment/min/locales.min';
