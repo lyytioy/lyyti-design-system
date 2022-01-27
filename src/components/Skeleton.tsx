882ebb434c8592c78907f989181e895932260fed
Petro Silenius <petro.silenius@lyyti.com>
2022-01-27 15:39:55 +0200

    Migrate Skeleton from makestyles to sx prop

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,16 +1,5 @@
-import { Theme } from '@mui/material';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { Skeleton as MuiSkeleton } from '@mui/material';
 import { SkeletonProps as MuiSkeletonProps } from '@mui/material';
-
-const useStyles = makeStyles<Theme>((theme) =>
-  createStyles({
-    root: {
-      backgroundColor: theme.palette.action.hover,
-    },
-  })
-);
 
 export type SkeletonProps = MuiSkeletonProps;
 
@@ -19,14 +8,12 @@ const Skeleton = ({
   variant = 'text',
   ...props
 }: SkeletonProps): JSX.Element => {
-  const classes = useStyles();
-
   return (
     <MuiSkeleton
       animation={animation}
       variant={variant}
+      sx={{ backgroundColor: 'action.hover' }}
       {...props}
-      classes={{ root: classes.root }}
     />
   );
 };
