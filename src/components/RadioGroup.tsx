a302423af8b9b69b674790754bcae353863f51cb
kacperkosinski <ext-kacper.kosinski@lyyti.com>
2022-06-09 10:30:17 +0200

    wrap component with forwardRef

--- src/components/RadioGroup.tsx
+++ src/components/RadioGroup.tsx
@@ -1,9 +1,10 @@
 import { RadioGroup as MuiRadioGroup, RadioGroupProps as MuiRadioGroupProps } from '@mui/material';
+import { forwardRef, Ref } from 'react';
 
 export interface RadioGroupProps extends MuiRadioGroupProps {}
 
-const RadioGroup = (props: RadioGroupProps): JSX.Element => {
-  return <MuiRadioGroup {...props} />;
+const RadioGroup = (props: RadioGroupProps, ref: Ref<HTMLDivElement>): JSX.Element => {
+  return <MuiRadioGroup ref={ref} {...props} />;
 };
 
-export default RadioGroup;
+export default forwardRef(RadioGroup);
