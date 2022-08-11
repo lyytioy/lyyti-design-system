fa2990095a21dfce12b505719238a61b45e310f8
kacperkosinski <ext-kacper.kosinski@lyyti.com>
2022-08-11 10:31:32 +0200

    add story for table component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,30 +1,50 @@
-import { Table as MuiTable } from '@mui/material';
+import { Table as MuiTable, TableContainer } from '@mui/material';
 import { TableProps as MuiTableProps } from '@mui/material/Table/Table';
 import { forwardRef, Ref } from 'react';
+import { TableContainerProps } from '@mui/material/TableContainer';
 
 export interface TableProps extends MuiTableProps {
   ref?: Ref<HTMLTableElement>;
+  height?: string;
+  color?: string;
+  containerProps?: TableContainerProps;
 }
 
-const Table = ({ children, sx, ...props }: TableProps, ref: Ref<HTMLTableElement>) => {
+const Table = (
+  {
+    children,
+    size,
+    sx,
+    containerProps,
+    height = '100%',
+    color = 'balticSea.50',
+    ...props
+  }: TableProps,
+  ref: Ref<HTMLTableElement>
+) => {
   return (
-    <MuiTable
-      ref={ref}
-      {...props}
-      sx={{
-        '.MuiTableCell-root': { border: 'none', padding: '14px' },
-
-        '.MuiTableBody-root': {
-          '.MuiTableRow-root': {
-            '&:nth-child(even)': { backgroundColor: 'light.light' },
-            '&:nth-child(odd)': { backgroundColor: 'balticSea.50' },
+    <TableContainer {...containerProps} sx={{ height, ...containerProps?.sx }}>
+      <MuiTable
+        ref={ref}
+        {...props}
+        sx={{
+          '.MuiTableCell-root': {
+            border: 'none',
+            padding: size === 'small' ? '6px' : '14px',
           },
-        },
-        ...sx,
-      }}
-    >
-      {children}
-    </MuiTable>
+
+          '.MuiTableBody-root': {
+            '.MuiTableRow-root': {
+              '&:nth-child(even)': { backgroundColor: 'light.light' },
+              '&:nth-child(odd)': { backgroundColor: color },
+            },
+          },
+          ...sx,
+        }}
+      >
+        {children}
+      </MuiTable>
+    </TableContainer>
   );
 };
 export default forwardRef(Table);
