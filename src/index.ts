02bed471e2b5464baaa09c39ff54d30b3e1b2182
kacperkosinski <ext-kacper.kosinski@lyyti.com>
2022-08-12 09:05:46 +0200

    reexport useTheme hook

--- src/index.ts
+++ src/index.ts
@@ -70,3 +70,4 @@ export { default as TableBody } from '@mui/material/TableBody';
 export { default as TableCell } from '@mui/material/TableCell';
 export { default as TableFooter } from '@mui/material/TableFooter';
 export { default as TableRow } from '@mui/material/TableRow';
+export { useTheme } from '@mui/system';
