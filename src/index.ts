bf1416dd80c30d0072409f0bfdaff13998b728b9
Mikael Järvinen <61495436+mikael-jarvinen@users.noreply.github.com>
2022-08-16 09:24:49 +0300

    Remove theme export

--- src/index.ts
+++ src/index.ts
@@ -71,4 +71,3 @@ export { default as TableCell } from '@mui/material/TableCell';
 export { default as TableFooter } from '@mui/material/TableFooter';
 export { default as TableRow } from '@mui/material/TableRow';
 export { useTheme } from '@mui/system';
-export { default as theme } from './themes/theme';
