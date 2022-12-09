c6477b26bc7cdf029584fb777fc67267f8067d82
Mikael Järvinen <mikael.jarvinen@lyyti.com>
2022-12-09 14:05:11 +0200

    feat: Upgrade storybook to v7:beta

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
+  core: {},
   staticDirs: ['../public'],
-  features: {
-    previewMdx2: true,
-  },
+  docs: {
+    docsPage: 'automatic'
+  }
 };
