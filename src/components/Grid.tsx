3bda5836c1e2158564679d0f84754b8ab73d4b3c
Petro Silenius <petro.silenius@lyyti.com>
2022-12-29 14:50:54 +0200

    Forward ref in Grid component

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,8 +1,9 @@
 import { Grid as MuiGrid, GridProps as MuiGridProps } from '@mui/material';
+import { forwardRef, Ref } from 'react';
 
 export type GridProps = MuiGridProps;
 
-const Grid = (props: GridProps): JSX.Element => {
-  return <MuiGrid {...props} />;
+const Grid = (props: GridProps, ref: Ref<HTMLDivElement>): JSX.Element => {
+  return <MuiGrid ref={ref} {...props} />;
 };
-export default Grid;
+export default forwardRef(Grid);
