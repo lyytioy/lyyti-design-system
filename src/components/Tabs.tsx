2f929173614a73764ee844a45b7f83a1c48a6555
Susanna Landström <susanna.landstrom@lyyti.com>
2023-01-30 10:18:27 +0200

    update tab styles

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,9 +1,32 @@
 import { Tabs as MuiTabs, TabsProps as MuiTabsProps } from '@mui/material';
 
-export type TabsProps = MuiTabsProps;
+export interface TabsProps extends Omit<MuiTabsProps, 'textColor' | 'indicatorColor'> {
+  color?: 'sky' | 'secondary';
+}
 
-const Tabs = (props: TabsProps): JSX.Element => {
-  return <MuiTabs {...props} />;
+const Tabs = ({ color = 'sky', sx = {}, ...props }: TabsProps): JSX.Element => {
+  const tabColor = color === 'sky' ? 'sky.500' : 'secondary.main';
+
+  return (
+    <MuiTabs
+      sx={{
+        '& .MuiTab-root': {
+          color: tabColor,
+          '&.Mui-disabled': {
+            color: 'text.disabled',
+          },
+          '&.Mui-selected': {
+            color: tabColor,
+            fontWeight: 'bold',
+          },
+        },
+        '& .MuiTabs-indicator': {
+          bgcolor: tabColor,
+        },
+      }}
+      {...props}
+    />
+  );
 };
 
 export default Tabs;
