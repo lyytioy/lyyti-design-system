1a0c268c2bd7cfc16b37682aecd578586d220252
Arkadiusz Juszczyk <ext-arkadiusz.juszczyk@lyyti.com>
2024-01-03 13:11:12 +0100

    updated participant icons

--- src/icons/ParticipantNotReacted.tsx
+++ src/icons/ParticipantNotReacted.tsx
@@ -5,11 +5,13 @@ const ParticipantNotReacted = (props: any) =>
     SvgIcon,
     props,
     <path
-      stroke="#7A8389"
+      className="participants_not_reacted_svg"
+      d="M7.7 9.2a4.1 4.1 0 1 0 0-8.2 4.1 4.1 0 0 0 0 8.2ZM12.4 12.6c-2.7-2.6-6.9-2.6-9.5.1C1.7 14 1 15.7 1 17.4"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={2}
-      d="M7.7 9.2a4.1 4.1 0 1 0 0-8.2 4.1 4.1 0 0 0 0 8.2ZM12.4 12.6c-2.7-2.6-6.9-2.6-9.5.1C1.7 14 1 15.7 1 17.4"
     />,
     <path
       fill="#1F98CD"
